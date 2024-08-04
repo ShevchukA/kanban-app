f70be47c6efbafaba8aa80d07c2a17370ded2088
ShevchukA <antoshka.vbg@mail.ru>
2024-08-04 12:48:53 +0700

    feat: add dragndrop for cards

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -6,23 +6,104 @@ import { Column, NewColumn } from '../../components/Column/Column';
 import { Board as BoardType } from '../../models/board';
 import { Column as ColumnType } from '../../models/column';
 import { Message } from '../../components/Message/Message';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import useBoardsMutation, { Action } from '../../hooks/useBoardsMutation';
 
 const Board = () => {
   const { activeBoardIndex } = useContext(UiContext);
   const queryClient = useQueryClient();
+  const editBoard = useBoardsMutation(Action.EditBoard);
+
   const boards: BoardType[] = queryClient.getQueryData(['boards']) ?? [];
 
-  const onDragEnd = useCallback(() => {
-    console.log('drag end');
-  }, []);
+  const onDragEnd = useCallback(
+    (result: DropResult) => {
+      const board = boards[activeBoardIndex];
+      const columns: ColumnType[] = board.columns;
+      const { destination, source } = result;
+
+      if (!destination) {
+        return;
+      }
+
+      if (
+        destination.droppableId === source.droppableId &&
+        destination.index === source.index
+      ) {
+        return;
+      }
+
+      const startColumn = columns.find(
+        (column) => column.id === source.droppableId
+      );
+      const endColumn = columns.find(
+        (column) => column.id === destination.droppableId
+      );
+
+      // card move inside the column
+      if (startColumn && startColumn === endColumn) {
+        const newTasks = Array.from(startColumn.tasks);
+        const [movedTask] = newTasks.splice(source.index, 1);
+        newTasks.splice(destination.index, 0, movedTask);
+
+        const newColumn: ColumnType = {
+          ...startColumn,
+          tasks: newTasks,
+        };
+
+        const newBoards = boards.map((board: BoardType) => {
+          board.columns = board.columns.map((column: ColumnType) => {
+            return column.id === newColumn.id ? newColumn : column;
+          });
+          return board;
+        });
+
+        editBoard.mutate(newBoards);
+      }
+      // card move to another column
+      else if (startColumn && endColumn) {
+        const startTasks = Array.from(startColumn.tasks);
+        const endTasks = Array.from(endColumn.tasks);
+        const [movedTask] = startTasks.splice(source.index, 1);
+        endTasks.splice(destination.index, 0, movedTask);
+
+        const newStartColumn: ColumnType = {
+          ...startColumn,
+          tasks: startTasks,
+        };
+
+        const newEndColumn: ColumnType = {
+          ...endColumn,
+          tasks: endTasks,
+        };
+
+        const newBoards = boards.map((board: BoardType) => {
+          board.columns = board.columns.map((column: ColumnType) => {
+            if (column.id === newStartColumn.id) {
+              return newStartColumn;
+            }
+            if (column.id === newEndColumn.id) {
+              return newEndColumn;
+            }
+            return column;
+          });
+          return board;
+        });
+
+        editBoard.mutate(newBoards);
+      } else {
+        console.log(`Drag'n'drop error`);
+      }
+    },
+    [boards, activeBoardIndex]
+  );
+
+  const board = boards[activeBoardIndex];
+  const columns: ColumnType[] = board.columns;
 
   if (boards.length === 0) {
     return <Message message='Create New Board to start...' />;
   }
-
-  const board = boards[activeBoardIndex];
-  const columns: ColumnType[] = board.columns;
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
